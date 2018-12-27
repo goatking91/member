2ff53d771da3cd865f37fc790283d8288fdef2b8
hohe12 <hohe123@naver.com>
2018-12-27 14:40:02 +0900

    메소드추가

--- bitboard/src/main/java/com/bit/member/controller/MemberController.java
+++ bitboard/src/main/java/com/bit/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.bit.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bit.member.model.MemberDto;
 import com.bit.member.service.MemberService;
 
 @Controller
@@ -13,10 +15,33 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	  
-	@RequestMapping("update")
-	public String updateMember(MemberDto memberDto) {
+	@RequestMapping("select")
+	public String selectMember(Model model) {
 	    
-		return "admin/boardmenu";
+		return "";
+	  }
+	
+	@RequestMapping("info")
+	public String infoMember(Model model) {
+	    
+		return "";
+	  }
+	
+	@RequestMapping("insert")
+	public String insertMember(MemberDto memberDto,Model model) {
+	    
+		return "";
 	  }
 
+	@RequestMapping("update")
+	public String updateMember(MemberDto memberDto,Model model) {
+	    
+		return "";
+	  }
+
+	@RequestMapping("delete")
+	public String deleteMember() {
+	    
+		return "";
+	  }
 }
